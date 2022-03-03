f6529ec6823d34c7eaed444fdcf443478dc41e63
Nduka <ugbehnduka4real@yahoo.com>
2022-03-03 23:38:22 +0100

    final commit

---
+++ src/components/Learn/Learn.js
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+import "./Learn.css";
+
+const Learn = () => {
+  return (
+    <div className="learn">
+   <Navbar />
+      <h1>Learn Page</h1>
+      <p>Under Construction</p>
+    </div>
+  );
+};
+
+export default Learn;
